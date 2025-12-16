85914aaf681173136345ab3a7d86716d2ec3a1b8
Masuduzzaman <md.santo0583210@gmail.com>
2025-12-17 04:40:58 +0600

    add register form and create api

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
 const express = require('express');
 const cors = require ('cors');
+require('dotenv').config();
+const port= process.env.PORT || 3000 ;
 
-const port
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://backend11:VOA8iQxWQP8lYYB7@cluster0.o6mocqo.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const database = client.db('bloodDonationDB')
+    const usersCollection = database.collection('users');
+
+    app.post('/users', async(req, res)=>{
+      const userInfo = req.body;
+      userInfo.role = "buyer";
+      userInfo.createdAt = new Date();
+
+      const result = await usersCollection.insertOne(userInfo);
+      res.send(result)
+    });
+
+    app.get(('/users/role/:email'),async(req,res)=>{
+      const {email} = req.params
+      const query = {email:email}
+      const result = await usersCollection.findOne(query)
+      res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res)=>{
+    res.send('hallo world')
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+})
